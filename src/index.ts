6aea7fd1b84b0caab8400d085a4af743d501dfb6
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-18 16:44:13 +0100

    initialized the prisma client

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 
 app.use("/api", rootRouter);
+
+export const prismaClient = new PrismaClient({
+  log: ["query"],
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
