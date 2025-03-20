f8acec9df67aa37b5420742fa9108ce3afb36f03
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-20 16:58:24 +0100

    created the error middleware and applied it to the index file

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import { PORT } from "./secrets";
 import rootRouter from "./routes";
 import { PrismaClient } from "@prisma/client";
-
+import { errorHandler } from "./middlewares/errors";
 const app = express();
 
 app.use(express.json());
@@ -12,6 +12,8 @@ app.use("/api", rootRouter);
 export const prismaClient = new PrismaClient({
   log: ["query"],
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
