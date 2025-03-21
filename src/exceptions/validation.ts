45f56379b940bfd7c9f1355058102fe45d39eb24
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-21 14:16:33 +0100

    type fix

--- src/exceptions/validation.ts
+++ src/exceptions/validation.ts
@@ -1,7 +1,7 @@
-import { HttpExceptions } from "./root";
+import { HttpExceptions, ErrorCodes } from "./root";
 
 export class UnprocessableEntity extends HttpExceptions {
-  constructor(error: any, message: string, errorCode: number) {
+  constructor(error: any, message: string, errorCode: ErrorCodes) {
     super(message, 422, errorCode, error);
   }
 }
