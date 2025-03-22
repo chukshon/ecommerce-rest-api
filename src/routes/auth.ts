26c28929452650e5d487d1871475aabea87349cb
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-22 20:53:15 +0100

    wrapped the sign up and login with the error handler higher order function

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
 import { signUp, login } from "../controllers/auth.controller";
+import { errorHandler } from "../error-handler";
 
 const authRoutes: Router = Router();
 
-authRoutes.post("/signup", signUp);
-authRoutes.post("/login", login);
+authRoutes.post("/signup", errorHandler(signUp));
+authRoutes.post("/login", errorHandler(login));
 
 export default authRoutes;
