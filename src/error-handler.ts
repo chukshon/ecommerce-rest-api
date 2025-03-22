0164336d08d1843a423feff4d50f9fba2dc477a9
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-22 20:52:28 +0100

    fully implemented the error handler

--- src/error-handler.ts
+++ src/error-handler.ts
@@ -1,12 +1,24 @@
-import { HttpExceptions } from "./exceptions/root";
+import { HttpExceptions, ErrorCodes } from "./exceptions/root";
+import { InternalException } from "./exceptions/internal-exception";
 import { Request, Response, NextFunction } from "express";
 
 export const errorHandler = (method: Function) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await method(req, res, next);
-    } catch (err) {
-      next(err);
+    } catch (err: any) {
+      let exception: HttpExceptions;
+      if (err instanceof HttpExceptions) {
+        exception = err;
+      } else {
+        exception = new InternalException(
+          "Something went wrong",
+          err,
+          ErrorCodes.INTERNAL_SERVER_ERROR
+        );
+      }
+
+      next(exception);
     }
   };
 };
