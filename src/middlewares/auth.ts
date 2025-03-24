960cf51cd27de2aa612c3e852e6f7978f0d9c9ea
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-24 17:02:00 +0100

    extract the token from the request headers

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,4 +5,12 @@ export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  // Extract the token from the request headers
+  // If no token is found, throw an UnauthorizedException
+  // If the token is found, verify it and extract the payload
+  // Get the user from the payload
+  // Attach the user to the current request object;
+
+  const token = req.headers.authorization;
+};
