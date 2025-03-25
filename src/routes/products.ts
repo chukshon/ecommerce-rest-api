8838a7f74b879f3258d0a4b51c96403588d10941
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-25 14:46:44 +0100

    added the admin middleware to the products route

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createProduct } from "../controllers/product.controller";
 import { authMiddleware } from "../middlewares/auth";
+import { adminMiddleware } from "../middlewares/admin";
 import { errorHandler } from "../error-handler";
 
 const productsRoutes: Router = Router();
 
-productsRoutes.post("/", [authMiddleware], errorHandler(createProduct));
+productsRoutes.post(
+  "/",
+  [authMiddleware, adminMiddleware],
+  errorHandler(createProduct)
+);
 
 export default productsRoutes;
