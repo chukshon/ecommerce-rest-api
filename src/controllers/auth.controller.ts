71a1b3da62db779044ea8b6e7e9cf87e41294fa8
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-25 05:09:25 +0100

    created the me controller for returning the authenticated user's information

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -63,3 +63,7 @@ export const login = async (req: Request, res: Response) => {
 
   res.json({ user, token });
 };
+
+export const me = async (req: Request, res: Response) => {
+  res.json(req.user);
+};
