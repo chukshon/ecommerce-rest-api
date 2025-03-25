b53bf546b1932c4b9a540b7e19a0479d289c2e64
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-25 14:34:05 +0100

    temporarily removed the auth middleware

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import { createProduct } from "../controllers/product.controller";
+import { authMiddleware } from "../middlewares/auth";
+import { errorHandler } from "../error-handler";
 
 const productsRoutes: Router = Router();
+
+productsRoutes.post("/", errorHandler(createProduct));
 
 export default productsRoutes;
