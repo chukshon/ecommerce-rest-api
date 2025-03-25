dafe09fe024590dd164213b9a2b67a0ebfe3a9d3
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-25 14:27:55 +0100

    finalized the create product controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,4 +4,12 @@ import { ProductSchema } from "../schema/product";
 
 export const createProduct = async (req: Request, res: Response) => {
   ProductSchema.parse(req.body);
+  const product = await prismaClient.product.create({
+    data: {
+      ...req.body,
+      tags: req.body.tags.join(","),
+    },
+  });
+
+  res.json(product);
 };
