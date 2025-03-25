2ee9ba57cb295279a6706af5288c26924bfccad7
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-25 05:12:36 +0100

    added the me route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,12 @@
 import { Router, Request, Response } from "express";
-import { signUp, login } from "../controllers/auth.controller";
+import { signUp, login, me } from "../controllers/auth.controller";
 import { errorHandler } from "../error-handler";
+import { authMiddleware } from "../middlewares/auth";
 
 const authRoutes: Router = Router();
 
 authRoutes.post("/signup", errorHandler(signUp));
 authRoutes.post("/login", errorHandler(login));
+authRoutes.get("/me", [authMiddleware], errorHandler(me));
 
 export default authRoutes;
