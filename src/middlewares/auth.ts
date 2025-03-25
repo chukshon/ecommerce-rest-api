f2af088c8001bee93ae06d166b9c76a0b83bb3b4
ChukwukaHonour <ChukwukaHonour@gmail.com>
2025-03-25 04:25:39 +0100

    check if the token is found,  verify and extract the payload

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,21 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import { UnauthorizedException } from "../exceptions/unauthorized";
 import { ErrorCodes } from "../exceptions/root";
+import { JWT_SECRET } from "../secrets";
+import jwt from "jsonwebtoken";
 
 export const authMiddleware = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  // Extract the token from the request headers
-  // If no token is found, throw an UnauthorizedException
-  // If the token is found, verify it and extract the payload
-  // Get the user from the payload
-  // Attach the user to the current request object;
-
   const token = req.headers.authorization;
 
   if (!token) {
     next(new UnauthorizedException("Unauthorized", ErrorCodes.UNAUTHORIZED));
   }
+
+  const payload = jwt.verify(token as string, JWT_SECRET);
 };
