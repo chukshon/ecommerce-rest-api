5a178192ca62e129e66137e157962cd61eb7f838
Chukwuka Honour <chukwukahonour@gmail.com>
2025-09-02 11:14:55 +0100

    Add AddressSchema using Zod for user address validation

---
+++ src/schema/users.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const AddressSchema = z.object({
+  lineOne: z.string(),
+  lineTwo: z.string().nullable(),
+  pinCode: z.string().length(6),
+  city: z.string(),
+  country: z.string(),
+  userId: z.number(),
+});
