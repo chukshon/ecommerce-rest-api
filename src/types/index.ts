256264acf8941b5832fa2820490b92280e3bf427
Chukwuka Honour <chukwukahonour@gmail.com>
2025-09-04 11:18:00 +0100

    Add ADDRESS_NOT_FOUND error code and message to ErrorCodes and ErrorMessages enums

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ export enum ErrorCodes {
   USER_NOT_FOUND = 1001,
   USER_ALREADY_EXISTS = 1002,
   INCORRECT_PASSWORD = 1003,
+  ADDRESS_NOT_FOUND = 1004,
   UNPROCESSABLE_ENTITY = 2001,
   INTERNAL_SERVER_ERROR = 3001,
   NOT_FOUND = 4001,
@@ -10,6 +11,7 @@ export enum ErrorCodes {
 
 export enum ErrorMessages {
   USER_NOT_FOUND = "User not found",
+  ADDRESS_NOT_FOUND = "Address not found",
   USER_ALREADY_EXISTS = "User already exists",
   INCORRECT_PASSWORD = "Incorrect password",
   UNPROCESSABLE_ENTITY = "Unprocessable entity",
