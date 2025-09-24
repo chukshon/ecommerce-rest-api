7240c2e6a6b19a0e75b4e9c88bc9e83b79cb1774
Chukwuka Honour <chukwukahonour@gmail.com>
2025-09-24 10:47:19 +0100

    minor edit

--- src/schema/users.ts
+++ src/schema/users.ts
@@ -8,7 +8,7 @@ export const AddressSchema = z.object({
   country: z.string(),
 });
 
-export const updateUserSchema = z.object({
+export const UpdateUserSchema = z.object({
   name: z.string().nullable(),
   defaultShippingAddress: z.number().nullable(),
   defaultBillingAddress: z.number().nullable(),
